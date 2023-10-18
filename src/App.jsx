a64c0a1d10d221465eb48b7156713fcfca486148
atunbi <damilareatunbi@gmail.com>
2023-10-19 00:19:49 +0100

    completed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "../src/components/header/Header";
+import Hero from "../src/components/hero/Hero";
+import Footer from "../src/components/footer/Footer";
+
+function App() {
+  return (
+    <div className="main">
+      <Header />
+      <Hero />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
